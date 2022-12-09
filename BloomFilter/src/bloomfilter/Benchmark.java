bb12b63306562f952dfe6f6514d1dc2dd200ea71
Crispel Clément <clement.crispel@gmail.com>
2022-12-09 14:49:48 +0100

    csv

--- BloomFilter/src/bloomfilter/Benchmark.java
+++ BloomFilter/src/bloomfilter/Benchmark.java
@@ -24,49 +24,28 @@ public class Benchmark {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random ran = new Random();
         try{
-            FileWriter outputFile = new FileWriter("test.csv");
-            outputFile.append("Iteration");
-            outputFile.append(",");
-            outputFile.append("Array");
-            outputFile.append(",");
-            outputFile.append("ArrayList");
-            outputFile.append(",");
-            outputFile.append("LinkedList");
-            outputFile.append("\n");
-            for(int i = 0; i< 1000; i++){
-                int tab[] = new int[TAB_VALUE];
-                for(int j = 0; j< tab.length-1; j++){
-                    tab[j] = abs(ran.nextInt());
-                    }
-                outputFile.append(""+(i+1));
-                outputFile.append(",");
-                outputFile.append(""+ArrayBenchmark(tab));
-                outputFile.append(",");
-                outputFile.append(""+ArrayListBenchmark(tab));
-                outputFile.append(",");
-                outputFile.append(""+LinkedListBenchMark(tab));
-                outputFile.append("\n");
-            }
-            outputFile.flush();
-            outputFile.close();
+            FileWriter outputFileContain = new FileWriter("contain.csv");
+            outputFileContain.append("Iteration");
+            outputFileContain.append(",");
+            outputFileContain.append("Array");
+            outputFileContain.append(",");
+            outputFileContain.append("ArrayList");
+            outputFileContain.append(",");
+            outputFileContain.append("LinkedList");
+            outputFileContain.append("\n");
+            Benchmark.addBenchmark(outputFileContain); 
+            outputFileContain.flush();
+            outputFileContain.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
     
-    private static long ArrayBenchmark(int tab[]){
+    private static long containArrayBenchmark(int tab[]){
         AbstractBloomFilter bloomFilter = new BloomFilterArray(N,K);
         long timeTab[] = new long[TAB_VALUE];
-        for(int i = 0; i< tab.length-1; i++){
-            long time = System.nanoTime();
-            bloomFilter.add(tab[i]);
-            timeTab[i] = System.nanoTime() - time;
-        }
-        //System.out.println("Array add : " + avg(timeTab) +" ns");
-        
         for(int i = 0; i< tab.length-1; i++){
             long time = System.nanoTime();
             bloomFilter.contain(tab[i]);
@@ -75,7 +54,41 @@ public class Benchmark {
         return avg(timeTab); 
     }
     
-    private static long ArrayListBenchmark(int tab[]){
+    private static long containArrayListBenchmark(int tab[]){
+        AbstractBloomFilter bloomFilter = new BloomFilterArrayList(N,K);
+        long timeTab[] = new long[TAB_VALUE];
+        for(int i = 0; i< tab.length-1; i++){
+            long time = System.nanoTime();
+            bloomFilter.contain(tab[i]);
+            timeTab[i] = System.nanoTime() - time;
+        }
+        return avg(timeTab); 
+        
+    }
+    
+    private static long containLinkedListBenchmark(int tab[]){
+        AbstractBloomFilter bloomFilter = new BloomFilterLinkedList(N,K);
+        long timeTab[] = new long[TAB_VALUE];
+        for(int i = 0; i< tab.length-1; i++){
+            long time = System.nanoTime();
+            bloomFilter.contain(tab[i]);
+            timeTab[i] = System.nanoTime() - time;
+        }
+        return avg(timeTab); 
+    }
+    
+        private static long addArrayBenchmark(int tab[]){
+        AbstractBloomFilter bloomFilter = new BloomFilterArray(N,K);
+        long timeTab[] = new long[TAB_VALUE];
+        for(int i = 0; i< tab.length-1; i++){
+            long time = System.nanoTime();
+            bloomFilter.add(tab[i]);
+            timeTab[i] = System.nanoTime() - time;
+        }
+        return avg(timeTab); 
+    }
+    
+    private static long addArrayListBenchmark(int tab[]){
         AbstractBloomFilter bloomFilter = new BloomFilterArrayList(N,K);
         
         long timeTab[] = new long[TAB_VALUE];
@@ -84,18 +97,11 @@ public class Benchmark {
             bloomFilter.add(tab[i]);
             timeTab[i] = System.nanoTime() - time;
         }
-        //System.out.println("ArrayList add : " + avg(timeTab) +" ns");
-        
-        for(int i = 0; i< tab.length-1; i++){
-            long time = System.nanoTime();
-            bloomFilter.contain(tab[i]);
-            timeTab[i] = System.nanoTime() - time;
-        }
         return avg(timeTab); 
         
     }
     
-    private static long LinkedListBenchMark(int tab[]){
+    private static long addLinkedListBenchmark(int tab[]){
         AbstractBloomFilter bloomFilter = new BloomFilterLinkedList(N,K);
        
         long timeTab[] = new long[TAB_VALUE];
@@ -104,15 +110,60 @@ public class Benchmark {
             bloomFilter.add(tab[i]);
             timeTab[i] = System.nanoTime() - time;
         }
-        //System.out.println("LinkedList add : " + avg(timeTab) +" ns");
-        
-        for(int i = 0; i< tab.length-1; i++){
-            long time = System.nanoTime();
-            bloomFilter.contain(tab[i]);
-            timeTab[i] = System.nanoTime() - time;
-        }
         return avg(timeTab); 
-        
+    }
+    
+    
+    private static void addBenchmark(FileWriter outputFileContain ){
+        Random ran = new Random();
+        try{
+            FileWriter outputFileAdd = new FileWriter("add.csv");
+            outputFileAdd.append("Iteration");
+            outputFileAdd.append(",");
+            outputFileAdd.append("Array");
+            outputFileAdd.append(",");
+            outputFileAdd.append("ArrayList");
+            outputFileAdd.append(",");
+            outputFileAdd.append("LinkedList");
+            outputFileAdd.append("\n");
+            for(int i = 0; i< 1000; i++){
+                int tab[] = new int[TAB_VALUE];
+                for(int j = 0; j< tab.length-1; j++){
+                    tab[j] = abs(ran.nextInt());
+                    }
+                outputFileAdd.append(""+(i+1));
+                outputFileAdd.append(",");
+                outputFileAdd.append(""+addArrayBenchmark(tab));
+                outputFileAdd.append(",");
+                outputFileAdd.append(""+addArrayListBenchmark(tab));
+                outputFileAdd.append(",");
+                outputFileAdd.append(""+addLinkedListBenchmark(tab));
+                outputFileAdd.append("\n");
+                Benchmark.containBenchmark(tab, i,outputFileContain);
+            }
+            outputFileAdd.flush();
+            outputFileAdd.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    private static void containBenchmark(int tab[], int i, FileWriter outputFileContain ){
+        try{
+            outputFileContain.append(""+(i+1));
+            outputFileContain.append(",");
+            outputFileContain.append(""+addArrayBenchmark(tab));
+            outputFileContain.append(",");
+            outputFileContain.append(""+addArrayListBenchmark(tab));
+            outputFileContain.append(",");
+            outputFileContain.append(""+addLinkedListBenchmark(tab));
+            outputFileContain.append("\n");    
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
     
     private static long avg(long tab[]){
